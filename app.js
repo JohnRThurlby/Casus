cc4bf363d077fb744b89d23c7bbe220565e04163
John Robert Thurlby <johnrthurlby@gmail.com>
2018-05-29 11:54:10 -0400

    changed config.json to config.js

--- app.js
+++ app.js
@@ -4,43 +4,44 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+
+const express = require("express")
+      bodyParser = require("body-parser")
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+var app = express()
+var PORT = process.env.PORT || 8080
 
 // Requiring our models for syncing
-var db = require("./models");
+var db = require("./models")
 
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars")
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
 // Routes
 // =============================================================
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app)
+require("./routes/api-routes.js")(app)
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+    console.log("App listening on PORT " + PORT)
+  })
+})
--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-{
+require("dotenv").config()
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": "Noelrr01",
+    "password": process.env.sql_Password,
     "database": "casus_db",
     "host": "127.0.0.1",
     "port": 3306,
@@ -16,11 +18,7 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "port": 3306,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "use_env_variable": "JAWSDB.URL"
   }
 }
