503daf23cdf0c8be1f57e94b8d72685613bce8ac
Anibal Pacheco <pachecol.anibala@gmail.com>
2018-05-29 18:22:53 -0400

    CSS updating. Trying to fix the CSS issue where its not linking

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 module.exports = {
   "development": {
     "username": "root",
-    "password": "null",
-    "database": "todolist",
+    "password": "Wed45tues!@",
+    "database": "casus_db",
     "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
