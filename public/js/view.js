865b4984b271d8aa013df2c69a001dc594675e01
Robert Olive <robert.aaron.olive@gmail.com>
2018-06-02 14:40:31 -0400

    placeholder event containers now organized in a grid. removed auto-generating event names for now.

--- public/js/view.js
+++ public/js/view.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+
+    var Event = function (title, image, description, location, start, end) {
+        this.title = title,
+        this.image = image,
+        this.description = description,
+        this.location = location,
+        this.start = start,
+        this.end = end,
+        this.liked = false,
+        this.create = function () {
+        }
+    }
+
   var feedPopulate = function () {
     //let query = document.getElementById("search").value
     
@@ -22,12 +36,12 @@ $(document).ready(function() {
     console.log(oData)
         // Note: this relies on the custom toString() methods below
 
-    for (var i = 0; i < 10; i++) {
-        feed.append( oData.events.event[i].title)
-        feed.append( oData.events.event[i].title)
-        feed.append( oData.events.event[i].title)
-        feed.append( oData.events.event[i].title)
-    }
+    // for (var i = 0; i < 10; i++) {
+    //     feed.append( oData.events.event[i].title)
+    //     feed.append( oData.events.event[i].title)
+    //     feed.append( oData.events.event[i].title)
+    //     feed.append( oData.events.event[i].title)
+    // }
 
     });
 }
