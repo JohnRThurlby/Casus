f63aaeac1f7c645f330f1dbbe3ec8cef04888ee1
John Robert Thurlby <johnrthurlby@gmail.com>
2018-06-05 19:43:19 -0400

    Update index

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,13 +16,7 @@ const db = require("../models"),
 
 var express = require("express");
 
-var router = express.Router();
 
-router.get("/", function(req, res) {
-
-    res.render("index", events[0]);
-  
-});
 
 // Routes
 // =============================================================
@@ -46,8 +40,8 @@ module.exports = function(app) {
       }
     }).then(function(dbUsers) {
       // We have access to the users as an argument inside of the callback function
-      res.json(dbUsers)
-      // res.render("index", events[0])
+      //res.json(dbUsers)
+      res.render("index", events[0])
     })
 
   })
@@ -56,8 +50,8 @@ module.exports = function(app) {
     // findOne returns the entry from a table for a specific user
     db.Users.findOne({
       where: {
-        email: req.query.Email,
-        password: req.query.Password
+        email: req.query.Email2,
+        password: req.query.Password2
       }
     }).then(function(dbUsers) {
       console.log(dbUsers)
