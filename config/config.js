812a0b8bfce1486d506d40e66a1d7cfe2fe786e6
aalkowni90 <aalkowni@gmail.com>
2018-06-05 17:25:09 -0400

    added background color for better visibility

--- config/config.js
+++ config/config.js
@@ -4,7 +4,6 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "randomAA12",
-    "password": "root",
     "database": "casus_db",
     "host": "127.0.0.1",
     "port": 3306,
