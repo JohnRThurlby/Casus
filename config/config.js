93db6d5394602ac30d51ef15faac35bff64fca4f
Anibal Pacheco <pachecol.anibala@gmail.com>
2018-06-05 18:29:19 -0400

    random update

--- config/config.js
+++ config/config.js
@@ -3,8 +3,7 @@ require("dotenv").config()
 module.exports = {
   "development": {
     "username": "root",
-    "password": "randomAA12",
-    "password": "root",
+    "password": "Wed45tues!@",
     "database": "casus_db",
     "host": "127.0.0.1",
     "port": 3306,
