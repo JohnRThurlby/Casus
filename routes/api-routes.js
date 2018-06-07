bf0567e27a336e5cb397ce858be871623acfcb62
Anibal Pacheco <pachecol.anibala@gmail.com>
2018-06-07 18:17:38 -0400

    Finished the Create Event Categories

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,30 +9,49 @@ const db = require("../models"),
       postcode = require('postcode-validator'),
       validator = require("email-validator"),
       ValidatePassword = require('validate-password'),
-      validPass = new ValidatePassword(),
-      unirest = require('unirest')
+      validPass = new ValidatePassword()
       
       
 
 var express = require("express");
 
+var unirest = require("unirest");
+
+var xml2js = require('xml2js');
+
 var objectEv = {
-  events:
-  [{
-  title: "Placeholder event",
-  image: "imgsrc",
-  description: "this is the event description",
-  start: "August 20, 2018",
-  end: "August 21, 2018"
-},
-{
-  title: "placeholder2",
-  image: "image2",
-  description: "description2",
-  start: "start2",
-  end: "end2"
-}]
-}
+  event:
+  [
+    // {
+//   title: "Placeholder event",
+//   image: "imgsrc",
+//   description: "this is the event description",
+//   start_time: "August 20, 2018",
+//   stop_time: "August 21, 2018"
+// },
+// {
+//   title: "placeholder2",
+//   image: "image2",
+//   description: "description2",
+//   start_time: "start2",
+//   stop_time: "end2"
+// }
+]
+};
+
+// These code snippets use an open-source library. http://unirest.io/nodejs
+unirest.get("https://community-eventful.p.mashape.com/events/search?app_key=kZVX6GMpxCX83vh9&location=orlando")
+.header("X-Mashape-Key", "35ZWjjUvuxmshz4RIV2HACPs4csep18CfcAjsnas8mTje72Nko")
+.header("Accept", "text/plain")
+.end(function (result) {
+  // console.log(result.status, result.headers, result.body);
+  var parseString = require('xml2js').parseString;
+  parseString(result.body, function (err, results) {
+    let eventful = results.search.events[0].event;
+    objectEv.events = objectEv.event.concat(eventful);
+    
+  });
+});
 
 // Routes
 // =============================================================
@@ -41,10 +60,18 @@ module.exports = function(app) {
 
 
 
-  // GET route for landing page 
+  // GET route for getting a specific users
   app.get("/", function(req, res) {
-    // findOne returns the entry from a table for a specific user
-      res.render("index")
+    
+    db.Users.findOne({
+      where: {
+        id: req.params.userid
+      }
+    }).then(function(dbUsers) {
+      // We have access to the users as an argument inside of the callback function
+    // getEvents()
+      // this renders our handlebar template with the event object.
+      res.render("index", objectEv)
   })
   
   // GET route for getting a specific users
@@ -58,13 +85,8 @@ module.exports = function(app) {
     }).then(function(dbUsers) {
       console.log(dbUsers)
       if (dbUsers != null) {
-        unirest.get("https://community-eventful.p.mashape.com/events/search?app_key=kZVX6GMpxCX83vh9&keywords=events%2C+orlando")
-        .header("X-Mashape-Key", "R0VKbqu0ffmshY41Oe1MS86gZyqQp1dLAMFjsn5f48sac9Uosu")
-        .header("Accept", "text/plain")
-        .end(function (result) {
-          console.log(result.status, result.headers, result.body);
-        });
       // We have access to the users as an argument inside of the callback function
+      // getEvents()
          res.render("partials/feeds/feeds")
       }
       else {
@@ -179,7 +201,12 @@ module.exports = function(app) {
         zipcode: req.body.Zipcode
       }).then(function(dbUsers) {
         // We have access to the new user as an argument inside of the callback function
-        res.render("index")
+
+
+        // getEvents();
+
+
+        res.render("index", objectEv)
       })
     }
     else {
@@ -236,4 +263,4 @@ module.exports = function(app) {
       res.json(dbUserlikes)
     })
   })
-}
+})}
